5f8dc785eb3454b0a02e614530cf41c4af312e7d
Kunall Banerjee <hey@kimchiii.space>
2025-06-16 08:38:41 -0400

    feat(darwin): add `KernelBootTime`

--- darwin/kernel.go
+++ darwin/kernel.go
@@ -3,12 +3,35 @@ package darwin
 import (
 	"fmt"
 	"syscall"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
 
 const (
+	kernelBootTimeMIB  = "kern.boottime"
 	kernelHostNameMIB  = "kern.hostname"
 	kernelOSReleaseMIB = "kern.osrelease"
 )
 
+func KernelBootTime() (time.Duration, error) {
+	bootTime, err := unix.SysctlRaw(kernelBootTimeMIB)
+	var timeval syscall.Timeval
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to get kernel clockrate: %w", err)
+	}
+
+	if len(bootTime) != int(unsafe.Sizeof(timeval)) {
+		return 0, fmt.Errorf("unexpected output of sysctl kern.boottime: %v (len: %d)", bootTime, len(bootTime))
+	}
+
+	timeval = *(*syscall.Timeval)(unsafe.Pointer(&bootTime[0]))
+	sec, nsec := timeval.Unix()
+
+	return time.Since(time.Unix(sec, nsec)), nil
+}
 
 func KernelHostName() (string, error) {
 	hostName, err := syscall.Sysctl(kernelHostNameMIB)
